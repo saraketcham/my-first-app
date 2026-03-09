f87a4909646de4316b05c4908726d752ccdd3036
sara.ketcham <sara.ketcham@gmail.com>
2026-03-08 20:48:40 -0700

    prettify code

--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -5,11 +5,10 @@ function ProfileCard({cardEmoji,cardHeader,cardSubtitle,cardBlurb}){
         <div className="m-6 border p-3 ">
             <span className="text-5xl"> {cardEmoji} </span>
             <div>
-            <div 
-            className=" inline-block px-3 my-3  font-bold border rounded-3xl shadow-md">
-            <h1 className="text-2xl" > {cardHeader}</h1>
-            <h3 className="text-med"> {cardSubtitle}</h3>  
-            </div>
+                <div className=" inline-block px-3 my-3  font-bold border rounded-3xl shadow-md">
+                    <h1 className="text-2xl" > {cardHeader}</h1>
+                    <h3 className="text-med"> {cardSubtitle}</h3>  
+                </div>
             </div>
             <div className="border  rounded-3xl p-5">
                 
